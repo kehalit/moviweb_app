a90847fcc5cbb75c15e624b386cc5a1d05876c4b
kehalit <kahineb.kn@gmail.com>
2025-03-25 10:25:55 +0100

    Implement SQLiteDataManager with CRUD operations for users and movies

--- datamanager/sqllite_data_magager.py
+++ datamanager/sqllite_data_magager.py
@@ -1,30 +1,75 @@
+import os
+from tkinter.font import names
+
 from flask_sqlalchemy import SQLAlchemy
 from datamanager.data_manager_interface import DataManagerInterface
+from data_models import db, User, Movie
+
+
+# Database configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+database_path = os.path.join(basedir, 'data', 'movies.db')
 
 class SQLiteDataManager(DataManagerInterface):
-    def __init__(self, db_file_name):
-        self.db = SQLAlchemy(db_file_name)
+    def __init__(self, app, db_file_name = "movies.db"):
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        db.init_app()
+
+        with app.app_context():
+            db.create_all()
 
     def get_all_users(self):
         #return all users
-        pass
+        return User.query.all()
 
     def get_user_movies(self, user_id):
         #return all movie list
-        pass
+        user = User.query.get(user_id) #!
+        return user.movies if user else None
 
-    def add_movie(self, user):
+
+    def add_user(self, name):
         #add new user to db
-        pass
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
 
-    def add_movie(movie):
+    def add_movie(self, user_id, movie_name, director, year, rating):
         # add new movie to db
-        pass
+        user = User.query.get(user_id)
+        if not user:
+            return None
+        new_movie = Movie(movie_name = movie_name, director=director, year=year, rating=rating, user_id=user_id)
+        db.session.add(new_movie)
+        db.session.commit()
+        return new_movie
 
-    def update_movie(movie):
+    def update_movie(self, movie_id , movie_name=None, director=None, year=None, rating=None):
         #update details of a specific movie in db
-        pass
+        movie = Movie.query.get(movie_id)
+        if not movie:
+            return None
+        if movie_name:
+            movie.movie_name = movie_name
+        if director:
+            movie.director = director
+        if year:
+            movie.year = year
+        if rating:
+            movie.rating = rating
 
-    def delete_movie(movie_id):
+        db.session.commit()
+        return movie
+
+
+    def delete_movie(self, movie_id):
         #delete specific movie from db
-        pass
+        movie = Movie.query.get(movie_id)
+        if not movie:
+            return False
+
+        db.session.delete(movie)
+        db.session.commit()
+        return True
