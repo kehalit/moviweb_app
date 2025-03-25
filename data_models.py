242229041c673cb2d7b5eaecab80f2c29d18458b
kehalit <kahineb.kn@gmail.com>
2025-03-25 12:30:50 +0100

    ammend the relationship between users and movies table

--- data_models.py
+++ data_models.py
@@ -10,23 +10,23 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     movies = db.relationship('Movie', backref= 'user', cascade = "all, delete-orphan")
 
     def __str__(self):
-        return f"users(id = {self.id}, name = {self.name})"
+        return f"users(user_id = {self.user_id}, name = {self.name})"
 
 class Movie(db.Model):
 
     __tablename__ = 'movies'
 
-    id = db.Column(db.Integer, primary_key=True, authoincrement=True)
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     movie_name = db.Column(db.String(255), nullable=False)
     director = db.Column(db.String(255), nullable = False)
-    year=db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
     def __str__(self):
         return (
