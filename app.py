09f8cf14bfe5c7f3e82653203e729994bd4cf850
kehalit <kahineb.kn@gmail.com>
2025-03-27 13:32:36 +0100

    modified update_movie route to check if any of the movie's details have changed.

--- app.py
+++ app.py
@@ -59,23 +59,50 @@ def add_movie(user_id):
     return render_template('add_movie.html', user=user)
 
 
-
-@app.route('/users/<user_id>/update_movie/<movie_id>', methods=['GET', 'POST'])
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
 def update_movie(user_id, movie_id):
+    # Fetch the movie details from the database
     movie = data_manager.get_movie(movie_id)
     if movie is None:
         return f"Movie with ID {movie_id} not found.", 404
 
     if request.method == 'POST':
-        movie.movie_name = request.form['name']  # Fix field names
-        movie.director = request.form['director']
-        movie.year = int(request.form['year'])
-        movie.rating = float(request.form['rating'])
+        # Fetch movie details from the API (for comparison)
+        movie_data_from_api = data_manager.fetch_movie_details(movie.movie_name)
 
-        data_manager.update_movie(movie)  # Save changes to DB
+        # Check if the user has modified the fields, if not we compare with API data
+        movie_name_changed = request.form['name'] != movie.movie_name
+        director_changed = request.form['director'] != movie.director
+        year_changed = int(request.form['year']) != movie.year
+        rating_changed = float(request.form['rating']) != movie.rating
+
+        if not (movie_name_changed or director_changed or year_changed or rating_changed):
+            # If nothing has changed, let's check with the OMDb API
+            if movie_data_from_api:
+                # If the API data is different, update the movie details from the API
+                if movie_data_from_api['movie_name'] != movie.movie_name:
+                    movie.movie_name = movie_data_from_api['movie_name']
+                if movie_data_from_api['director'] != movie.director:
+                    movie.director = movie_data_from_api['director']
+                if int(movie_data_from_api['year']) != movie.year:
+                    movie.year = int(movie_data_from_api['year'])
+                if float(movie_data_from_api['rating']) != movie.rating:
+                    movie.rating = float(movie_data_from_api['rating'])
+
+        else:
+            # If the user has changed anything, update it directly from the form
+            movie.movie_name = request.form['name']
+            movie.director = request.form['director']
+            movie.year = int(request.form['year'])
+            movie.rating = float(request.form['rating'])
+
+        # Commit the changes to the database
+        data_manager.update_movie(movie)
         return redirect(url_for('user_movies', user_id=user_id))
 
     return render_template('update_movie.html', movie=movie, user_id=user_id)
+
+
 
 
 @app.route('/users/<int:user_id>/delete_movie/<int:movie_id>', methods=['GET', 'POST'])
