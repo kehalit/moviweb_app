f9133185c592eaf6272e2897abeb6ff27e829308
kehalit <kahineb.kn@gmail.com>
2025-03-28 14:04:52 +0100

    new review table created to store user reviews of movies

--- data_models.py
+++ data_models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     movies = db.relationship('Movie', backref= 'user', cascade = "all, delete-orphan")
+    reviews = db.relationship('Review', backref= 'user', cascade = "all, delete-orphan")
 
     def __str__(self):
         return f"users(user_id = {self.user_id}, name = {self.name})"
@@ -27,10 +28,27 @@ class Movie(db.Model):
     year = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    reviews = db.relationship('Review', backref= 'movie', cascade = "all, delete-orphan")
 
     def __str__(self):
         return (
                 f"movies(id = {self.movie_id}, movie_name={self.movie_name},"
                 f"director={self.director}, year={self.year},"
                 f"rating= {self.rating})"
+        )
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    review_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable = False)
+    review_text = db.Column(db.Text,  nullable=True)
+    rating = db.Column(db.Float, nullable=False)
+
+
+    def __str__(self):
+        return (
+                f"review(id= {self.review_id}, review_text={self.review_text},"
+                f"rating={self.rating})"
         )
